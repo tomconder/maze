f0ac8fc6f49625e3fafef3525c3a93a1ace42ee8
Tom Conder <tom@itstom.co>
2023-02-12 01:49:47 -0600

    feat(pch): add pre-compiled headers (#12)

---
+++ sponge/src/spongepch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "SDL.h"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
