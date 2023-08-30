85336936863d7fce7afada67ff9aa32283f8b1d5
Tom Conder <tom@itstom.co>
2023-08-30 00:12:06 -0500

    Revert "feat(cxx): use std::stacktrace"
    This reverts commit f58abf3421db60147a478764582ac880800bdb15.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,7 +5,6 @@
 #include "maze.h"
 #include "sponge.h"
 #include "version.h"
-#include <stacktrace>
 
 const std::unique_ptr<Maze> maze = std::make_unique<Maze>();
 constexpr std::string_view spongeLogFile = "log.txt";
@@ -27,8 +26,6 @@ bool startup() {
     std::string appName = ss.str();
 
     SPONGE_INFO("{}", appName);
-
-    std::cout << std::stacktrace::current() << std::flush;
 
 #ifdef EMSCRIPTEN
     uint32_t width = 800;
