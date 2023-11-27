4dad512b430d3e43f205a3b62730b408e2229d35
Tom Conder <tom@itstom.co>
2023-11-26 18:06:49 -0600

    feat(log): create openglinfo (#42)

--- sponge/src/renderer/opengl/openglcontext.h
+++ sponge/src/renderer/opengl/openglcontext.h
@@ -2,7 +2,7 @@
 
 #include "glm/vec4.hpp"
 #include "renderer/graphicscontext.h"
-#include "renderer/opengl/gl.h"
+#include <SDL.h>
 #include <string>
 
 struct SDL_Window;
@@ -11,20 +11,12 @@ namespace sponge {
 
 class OpenGLContext : public GraphicsContext {
    public:
-    OpenGLContext(SDL_Window* window, std::string name);
+    explicit OpenGLContext(SDL_Window* window);
     ~OpenGLContext() override;
 
     void flip(void* window) override;
 
-    void logGlVersion() const;
-
     void toggleFullscreen(void* window);
-
-    static void logGraphicsDriverInfo();
-
-    static void logOpenGLContextInfo();
-
-    void logStaticOpenGLInfo() const;
 
    private:
     int width = 0;
