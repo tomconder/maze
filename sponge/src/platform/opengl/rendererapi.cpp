3f323b0ff76130dcce347c34aa36acea6ae6b198
Tom Conder <tom@itstom.co>
2024-05-29 15:18:12 -0500

    fix(opengl): fix lint issue

--- sponge/src/platform/opengl/rendererapi.cpp
+++ sponge/src/platform/opengl/rendererapi.cpp
@@ -3,10 +3,10 @@
 #include "logging/log.hpp"
 #include "platform/opengl/gl.hpp"
 
-static void APIENTRY glLogMessage(GLenum source, GLenum type, uint32_t id,
-                                  GLenum severity, GLsizei length,
-                                  const GLchar* message,
-                                  const void* userParam) {
+void APIENTRY glLogMessage(GLenum source, GLenum type, uint32_t id,
+                           GLenum severity, GLsizei length,
+                           const GLchar *message,
+                           const void *userParam) {
     std::string source_str;
     std::string type_str;
 
@@ -90,44 +90,44 @@ static void APIENTRY glLogMessage(GLenum source, GLenum type, uint32_t id,
 
 namespace sponge::platform::opengl {
 
-void RendererAPI::init() {
-    if (glDebugMessageCallback != nullptr) {
-        glDebugMessageCallback(glLogMessage, nullptr);
-        glEnable(GL_DEBUG_OUTPUT);
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        // do not want driver notification spam
-        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE,
-                              GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr,
-                              GL_FALSE);
+    void RendererAPI::init() {
+        if (glDebugMessageCallback != nullptr) {
+            glDebugMessageCallback(glLogMessage, nullptr);
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            // do not want driver notification spam
+            glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE,
+                                  GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr,
+                                  GL_FALSE);
+        }
+
+        glEnable(GL_MULTISAMPLE);
+
+        glDepthFunc(GL_LEQUAL);
+        glEnable(GL_DEPTH_TEST);
+
+        glDisable(GL_SCISSOR_TEST);
+        glDisable(GL_STENCIL_TEST);
+
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
+        glEnable(GL_CULL_FACE);
+
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_BLEND);
     }
 
-    glEnable(GL_MULTISAMPLE);
+    void RendererAPI::setViewport(const int32_t x, const int32_t y,
+                                  const int32_t width, const int32_t height) {
+        glViewport(x, y, width, height);
+    }
 
-    glDepthFunc(GL_LEQUAL);
-    glEnable(GL_DEPTH_TEST);
+    void RendererAPI::setClearColor(const glm::vec4 &color) {
+        glClearColor(color.r, color.g, color.b, color.a);
+    }
 
-    glDisable(GL_SCISSOR_TEST);
-    glDisable(GL_STENCIL_TEST);
-
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CCW);
-    glEnable(GL_CULL_FACE);
-
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
-}
-
-void RendererAPI::setViewport(const int32_t x, const int32_t y,
-                              const int32_t width, const int32_t height) {
-    glViewport(x, y, width, height);
-}
-
-void RendererAPI::setClearColor(const glm::vec4& color) {
-    glClearColor(color.r, color.g, color.b, color.a);
-}
-
-void RendererAPI::clear() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
+    void RendererAPI::clear() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
 
 }  // namespace sponge::platform::opengl
