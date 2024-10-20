829e4fd15bffec19f94057411b958d724102b77f
Tom Conder <tom@itstom.co>
2024-10-20 03:33:32 -0500

    feat(glfw): add default constructor

--- sponge/src/core/timer.hpp
+++ sponge/src/core/timer.hpp
@@ -6,10 +6,14 @@ constexpr double MICROSECONDS_TO_SECONDS = 1e-6F;
 
 namespace sponge::core {
 
+using std::chrono::high_resolution_clock;
+
 class Timer {
    public:
+    Timer() = default;
+
     void tick() {
-        auto currentTicks{ std::chrono::high_resolution_clock::now() };
+        auto currentTicks{ high_resolution_clock::now() };
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
             currentTicks - previousTicks);
         elapsedSeconds = duration.count() * MICROSECONDS_TO_SECONDS;
@@ -22,8 +26,8 @@ class Timer {
 
    private:
     double elapsedSeconds{ 0.0 };
-    std::chrono::high_resolution_clock::time_point previousTicks{
-        std::chrono::high_resolution_clock::now()
+    high_resolution_clock::time_point previousTicks{
+        high_resolution_clock::now()
     };
 };
 
