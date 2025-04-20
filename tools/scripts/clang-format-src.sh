cb57293ff8e77903b1654d70f28b793140f3f43a
Tom Conder <tom@itstom.co>
2025-04-20 18:03:37 -0500

    fix(build): simplify script

--- tools/scripts/clang-format-src.sh
+++ tools/scripts/clang-format-src.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-pushd . > /dev/null
-cd "../../sponge/src"
-echo "Running clang-format in $(pwd)"
-find . -regex '.*\.[chm]p*' -exec clang-format -i {} \;
-popd > /dev/null
-
-pushd . > /dev/null
-cd "../../game/src"
-echo "Running clang-format in $(pwd)"
-find . -regex '.*\.[chm]p*' -exec clang-format -i {} \;
-popd > /dev/null
+echo "Running clang-format"
+find ../../sponge/src ../../game/src -regex '.*\.[chm]p*' -exec clang-format -i --style=file {} \;
 
 echo "clang-format complete!"
