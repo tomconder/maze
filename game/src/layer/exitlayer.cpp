1b8eef98d28e455d1416fc63b0c7b9bee4740dd1
Tom Conder <tom@itstom.co>
2025-04-24 15:16:51 -0500

    fix(exit): update gamma

--- game/src/layer/exitlayer.cpp
+++ game/src/layer/exitlayer.cpp
@@ -89,7 +89,7 @@ bool ExitLayer::onUpdate(const double elapsedTime) {
     auto width = static_cast<float>(orthoCamera->getWidth());
     auto height = static_cast<float>(orthoCamera->getHeight());
 
-    quad->render({ 0.F, 0.F }, { width, height }, { 0.F, 0.F, 0.F, 0.85F });
+    quad->render({ 0.F, 0.F }, { width, height }, { 0.F, 0.F, 0.F, 0.56F });
 
     quad->render({ width * .23F, 0.F }, { width * .77F, height },
                  { .52F, .57F, .55F, 1.F });
