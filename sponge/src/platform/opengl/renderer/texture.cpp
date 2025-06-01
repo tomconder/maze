5ccb9ab2e837a0647f791de1775f3f77fb147542
Tom Conder <tom@itstom.co>
2025-06-01 15:56:17 -0500

    fix(texture): remove GL_REPEAT since it is the default

--- sponge/src/platform/opengl/renderer/texture.cpp
+++ sponge/src/platform/opengl/renderer/texture.cpp
@@ -48,8 +48,6 @@ void Texture::generate(const uint32_t textureWidth,
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                     GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
