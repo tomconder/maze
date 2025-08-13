17f02c622b6c4cf450ad9523ab5908bb17f7acc2
Tom Conder <tom@itstom.co>
2025-08-13 12:18:31 -0500

    fix(log): use windows path separator

--- sponge/src/platform/windows/core/winfile.cpp
+++ sponge/src/platform/windows/core/winfile.cpp
@@ -8,7 +8,7 @@ std::string WinFile::getLogDir(const std::string& app) {
     size_t sz = 0;
     if (_dupenv_s(&appdata, &sz, "LOCALAPPDATA") == 0 && appdata != nullptr) {
         const std::filesystem::path path(appdata);
-        return path.string() + "/" + app + "/";
+        return path.string() + "\\" + app + "\\";
     }
 
     throw std::runtime_error("Failed to get appdata folder");
