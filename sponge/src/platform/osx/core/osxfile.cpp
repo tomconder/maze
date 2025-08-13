b2a46d6a25da74f1257b52d6dab5bc53dda2330b
Tom Conder <tom@itstom.co>
2025-08-13 12:55:34 -0500

    fix(app): use filesystem operations

--- sponge/src/platform/osx/core/osxfile.cpp
+++ sponge/src/platform/osx/core/osxfile.cpp
@@ -2,11 +2,12 @@
 #include <CoreFoundation/CFBundle.h>
 #include <glob.h>
 #include <sysdir.h>
+#include <filesystem>
 
 namespace sponge::platform::osx::core {
 
 std::string OSXFile::expandTilde(const char* str) {
-    glob_t globbuf;
+    glob_t globbuf{};
     if (glob(str, GLOB_TILDE, nullptr, &globbuf) == 0) {
         std::string result(globbuf.gl_pathv[0]);
         globfree(&globbuf);
@@ -38,7 +39,8 @@ std::string OSXFile::getLogDir(const std::string& app) {
     auto state = sysdir_start_search_path_enumeration(
         SYSDIR_DIRECTORY_APPLICATION_SUPPORT, SYSDIR_DOMAIN_MASK_USER);
     if (sysdir_get_next_search_path_enumeration(state, path) != 0) {
-        return expandTilde(path) + "/" + app + "/";
+        const std::filesystem::path fpath(expandTilde(path));
+        return (fpath / app / "").string();
     }
 
     throw std::runtime_error("Failed to get settings folder");
