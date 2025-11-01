322d9b9b6a8b820bb8e5e598551f972c7014d699
Tom Conder <tom@itstom.co>
2025-11-01 01:27:08 -0500

    fix(imgui): replace input scalar with slider for shadow bias

--- game/src/layer/imgui/imguilayer.cpp
+++ game/src/layer/imgui/imguilayer.cpp
@@ -230,14 +230,12 @@ void ImGuiLayer::showDirectionalLightControls() {
             }
         });
 
-        constexpr float step = .001F;
-
         showTableRow([&] {
             ImGui::Text("Shadow Bias");
             ImGui::TableNextColumn();
             ImGui::SetNextItemWidth(ImGui::GetContentRegionAvail().x);
-            if (ImGui::InputScalar("##directionalbias", ImGuiDataType_Float,
-                                   &bias, &step)) {
+            if (ImGui::SliderFloat("##directionalbias", &bias, 0.001F, 0.03F,
+                                   "%.3f", ImGuiSliderFlags_AlwaysClamp)) {
                 mazeLayer->setShadowBias(bias);
             }
         });
