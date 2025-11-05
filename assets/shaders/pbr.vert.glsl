fab89d0de29930d64b57beb6da0f9351fcc3860a
Tom Conder <tom@itstom.co>
2025-11-05 16:42:49 -0600

    refactor(shader): consolidate vertex output structure for fragment shader

--- assets/shaders/pbr.vert.glsl
+++ assets/shaders/pbr.vert.glsl
@@ -4,23 +4,25 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 texCoord;
 layout (location = 2) in vec3 normal;
 
-out vec3 vPosition;
-out vec2 vTexCoord;
-out vec3 vNormal;
-out vec4 vFragPosLightSpace;
+out VS_OUT {
+    vec3 position;
+    vec3 normal;
+    vec2 texCoords;
+    vec4 fragPosLightSpace;
+} vs_out;
 
 uniform mat4 mvp;
 uniform mat4 model;
 uniform mat4 lightSpaceMatrix;
 
 void main() {
-    vec4 worldPos = model * vec4(position, 1.0);
     gl_Position = mvp * vec4(position, 1.0);
 
-    vPosition = worldPos.xyz;
-    vTexCoord = texCoord;
-    vNormal = mat3(transpose(inverse(model))) * normal;
+    vec4 worldPos = model * vec4(position, 1.0);
+    vs_out.position = worldPos.xyz;
+    vs_out.normal = transpose(inverse(mat3(model))) * normal;
+    vs_out.texCoords = texCoord;
 
     // transform to light space
-    vFragPosLightSpace = lightSpaceMatrix * worldPos;
+    vs_out.fragPosLightSpace = lightSpaceMatrix * worldPos;
 }
