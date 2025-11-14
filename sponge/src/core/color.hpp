5ecde3d0111b97407c958291aaab93182655932d
Tom Conder <tom@itstom.co>
2025-11-13 20:20:14 -0600

    refactor(color): use string_view for hex color functions

--- sponge/src/core/color.hpp
+++ sponge/src/core/color.hpp
@@ -2,16 +2,17 @@
 
 #include <glm/glm.hpp>
 #include <string>
+#include <string_view>
 
 namespace sponge::core {
 
 class Color {
 public:
-    static glm::vec3 hexToRGB(const std::string& hex);
+    static glm::vec3 hexToRGB(std::string_view hex);
     static void      rgbToHex(const glm::vec3& rgb, char* hex);
 
 private:
-    inline static float hexToFloat(const std::string& hex);
+    inline static float hexToFloat(std::string_view hex);
 };
 
 }  // namespace sponge::core
