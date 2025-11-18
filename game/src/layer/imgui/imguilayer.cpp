18b3d2688d2efbd562e4a97e7a1ca3a6d6b4fc09
Tom Conder <tom@itstom.co>
2025-11-18 16:53:47 -0600

    fix: avoid undefined behavior for non-ASCII characters

--- game/src/layer/imgui/imguilayer.cpp
+++ game/src/layer/imgui/imguilayer.cpp
@@ -634,8 +634,9 @@ bool ImGuiLayer::isCaseInsensitiveEqual(const std::string& str1,
                                         const std::string& str2) {
     return str1.size() == str2.size() &&
            std::equal(str1.begin(), str1.end(), str2.begin(),
-                      [](auto a, auto b) {
-                          return std::tolower(a) == std::tolower(b);
+                      [](char a, char b) {
+                          return std::tolower(static_cast<unsigned char>(a)) ==
+                                 std::tolower(static_cast<unsigned char>(b));
                       });
 }
 }  // namespace game::layer::imgui
