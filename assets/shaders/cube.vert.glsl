66bfc48412f29a47c95263f401e3e9660b31cb31
Tom Conder <tom@itstom.co>
2025-11-19 14:38:38 -0600

    fix(format): adjust layout syntax and spacing in GLSL files

--- assets/shaders/cube.vert.glsl
+++ assets/shaders/cube.vert.glsl
@@ -1,6 +1,6 @@
 #version 330 core
 
-layout (location = 0) in vec3 position;
+layout(location = 0) in vec3 position;
 
 uniform mat4 mvp;
 uniform vec3 lightColor;
@@ -9,5 +9,5 @@ out vec3 vColor;
 
 void main() {
     gl_Position = mvp * vec4(position, 1.0);
-    vColor = lightColor;
+    vColor      = lightColor;
 }
