b7d8653ebdd8dafab6e9aff4de155e6902a9283f
Tom Conder <tom@itstom.co>
2026-04-12 22:19:42 -0500

    fix(deps): reduce stb_image build footprint
    - Disable unused image formats (BMP, GIF, HDR, PIC, PNM, PSD, TGA).

--- sponge/deps/stb/stb_image.cpp
+++ sponge/deps/stb/stb_image.cpp
@@ -1,2 +1,9 @@
+#define STBI_NO_BMP
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STBI_NO_PSD
+#define STBI_NO_TGA
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
