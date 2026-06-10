4066cd21232c1dfd856866e4b101ecacae89ba70
Tom Conder <tom@itstom.co>
2026-06-09 19:34:28 -0500

    feat(shader): replace MSDF median with coverage alpha sampling

--- assets/shaders/glsl/text.frag.glsl
+++ assets/shaders/glsl/text.frag.glsl
@@ -6,16 +6,8 @@ in vec2 vTexCoord;
 
 uniform sampler2D text;
 uniform vec3      textColor;
-uniform float     screenPxRange;
-
-float median(float r, float g, float b) {
-    return max(min(r, g), min(max(r, g), b));
-}
 
 void main() {
-    vec3  msd              = texture(text, vTexCoord).rgb;
-    float sd               = median(msd.r, msd.g, msd.b);
-    float screenPxDistance = screenPxRange * (sd - 0.5);
-    float opacity          = clamp(screenPxDistance + 0.5, 0.0, 1.0);
-    FragColor = vec4(textColor, 1.0) * vec4(1.0, 1.0, 1.0, opacity);
+    float coverage = texture(text, vTexCoord).r;
+    FragColor = vec4(textColor, coverage);
 }
